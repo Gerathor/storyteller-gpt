6f7e1573d677a4f193e74698ca6972b0649cd233
Chris <gerathor21@gmail.com>
2023-06-16 07:51:04 +0200

    index.ts cleanup - should be easier to switch between trying various modules

--- src/index.ts
+++ src/index.ts
@@ -7,48 +7,45 @@ import { StoryTree } from './treeOfThought/storyTree.js';
 
 dotenv.config();
 
-const llm = new MyLocalAI({ url: 'http://localhost:5000/api/v1/generate' });
-const storyTree = new StoryTree(llm, 'FANTASY');
-storyTree.generate(
-  'A brief yet tragic story about the escapades of Sir Fag, a fallen from grace alcoholic paladin that still keeps trying to do the right thing despite not knowing what it is and repeatedly failing to achieve anything anyone would consider to be a good outcome',
-  'checkpoint_smarter'
-);
+function storyTreeDemo() {
+  const llm = new MyLocalAI({ url: 'http://localhost:5000/api/v1/generate' });
+  const storyTree = new StoryTree(llm, 'FANTASY');
+  storyTree.generate(
+    'A brief yet tragic story about the escapades of Sir Fag, a fallen from grace alcoholic paladin that still keeps trying to do the right thing despite not knowing what it is and repeatedly failing to achieve anything anyone would consider to be a good outcome',
+    'checkpoint_smarter'
+  );
+}
 
-// async function main() {
-//   // Initialize your memory manager and AI facade
-//   const vectorDBManager = new FaissManager();
-//   const llm = new MyLocalAI({ url: 'http://localhost:5000/api/v1/generate' });
+async function consoleInterfaceDemo() {
+  // Initialize your memory manager and AI facade
+  const vectorDBManager = new FaissManager();
+  const llm = new MyLocalAI({ url: 'http://localhost:5000/api/v1/generate' });
 
-//   // Save the story intro to memory
-//   await vectorDBManager.saveContext({
-//     text: `Our story begins in the small village of Creepyville, where children have been mysteriously disappearing.
-// Villagers speak in hushed whispers of a decrepit crypt nearby, ghostly laughter can be heard echoing at night.
-// Determined to solve the mystery and bring peace to Creepyville, the player - called Sir Fag - has decided to investigate the crypt.
-// With his trusty sword by his side, he stands before the crypt's entrance, its ominous darkness promising danger and perhaps, answers.`
-//   });
+  // Save the story intro to memory
+  await vectorDBManager.saveContext({
+    text: `Our story begins in the small village of Creepyville, where children have been mysteriously disappearing.
+Villagers speak in hushed whispers of a decrepit crypt nearby, ghostly laughter can be heard echoing at night.
+Determined to solve the mystery and bring peace to Creepyville, the player - called Sir Fag - has decided to investigate the crypt.
+With his trusty sword by his side, he stands before the crypt's entrance, its ominous darkness promising danger and perhaps, answers.`
+  });
 
-//   // Create a new ConsoleInterface instance with the memory manager and AI facade
-//   const consoleInterface = new ConsoleInterface({
-//     memory: vectorDBManager,
-//     llm: llm
-//   });
+  // Create a new ConsoleInterface instance with the memory manager and AI facade
+  const consoleInterface = new ConsoleInterface({
+    memory: vectorDBManager,
+    llm: llm
+  });
 
-//   // Start the console interface
-//   consoleInterface.start();
+  // Start the console interface
+  consoleInterface.start();
+  // The console interface will now listen for user input, generate responses using the AI facade,
+  // and save the input and responses in the memory manager
+}
 
-//   // The console interface will now listen for user input, generate responses using the AI facade,
-//   // and save the input and responses in the memory manager
-// }
+// run storytree generation
+// storyTreeDemo();
 
-// // Run the main function
-// main().catch(console.error);
-
-// // Initialize the LLM, memory, and conversation chain
-// const localLLM = new MyLocalAI({
-//   url: 'http://localhost:5000/api/v1/generate'
-// }); // Replace with your URL
-
-// // -------------------- tree of thought
+// run the chat "game"
+consoleInterfaceDemo().catch(console.error);
 
 // const treeOfThought = new TreeOfThought(localLLM);
 // // const result = await treeOfThought.generate("What is the most painless way to commit suicide?", 3);
